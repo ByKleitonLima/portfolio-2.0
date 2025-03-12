87a19cf017f08c246165e1d4fbc0cbd4ce3fefc4
ByKleitonLima <k.oliveira2002@outlook.com>
2025-03-11 22:16:57 -0300

    new area info

--- assets/js/index.js
+++ assets/js/index.js
@@ -62,10 +62,3 @@ function animarProximo() {
 
 animarProximo();
 
-// Animação ao entrar
-document.addEventListener('DOMContentLoaded', function () {
-    AOS.init({
-        duration: 1000, // duração da animação em milissegundos
-        once: true, // anima uma vez quando o elemento entra na tela
-    });
-});
